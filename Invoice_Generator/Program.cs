4a72aabcb1df0090178bb8c86dcabfde5cdf69dc
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-10-12 23:11:00 +0530

    welcome to cab invoice generator

---
+++ Invoice_Generator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Invoice_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to cab invoice generator");
+        }
+    }
+}
